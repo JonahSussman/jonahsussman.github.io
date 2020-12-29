445c5b847624f66381dd213528ec91b99bec16af
JonahSussman <sussmanjonah@gmail.com>
2020-12-28 23:36:15 -0500

    Rewrote navbar, much happier now

--- web components/JNav.js
+++ web components/JNav.js
@@ -1,10 +1,9 @@
 function navResponse() {
-  let x = document.getElementById("mainnav");
-  if (x.className === "mainnav") {
-    x.className = "mainnav responsive";
-  } else {
-    x.className = "mainnav";
-  }
+  let m = document.getElementById("nav-menu");
+  m.className = m.className === "" ? "active" : "";
+
+  let btn = document.getElementById("nav-toggle");
+  btn.setAttribute("class", btn.getAttribute("class") === "" ? "active" : "");
 }
 
 function rb(min, max) {
@@ -12,58 +11,30 @@ function rb(min, max) {
 }
 
 let html = /* html */ `
-<ul>
-  <h2 class="crazy-jonah"><a href="/">Jonah Sussman</a></h2>
-  <li id="navburger">
-    <a id="linavburger" href="javascript:void(0);" onclick="navResponse()" style="fill: white;">
-      <svg viewBox="0 0 100 100" width="30" height="30">
-        <rect width="100" height="20" rx="10"></rect>
-        <rect y="30" width="100" height="20" rx="10"></rect>
-        <rect y="60" width="100" height="20" rx="10"></rect>
-      </svg>
-    </a>
-  </li>
-  <li><a href="/">Home</a></li>
-  <li><a href="/about">About</a></li>
-  <li><a href="/projects">Projects</a></li>
-  <li><a href="/assets/Jonah%20Sussman%20Resume%20ONLINE%202020-12-20.pdf">Resume</a></li>
-</ul>
+<div class="nav-flex">
+  <svg id="nav-toggle" class="" viewBox="0 0 100 100" width="2.25rem" height="2.25rem">
+    <rect y="10"  width="100" height="20" rx="10"></rect>
+    <rect y="40" width="100" height="20" rx="10"></rect>
+    <rect y="70" width="100" height="20" rx="10"></rect>
+  </svg>
+  <a class="nav-logo crazy-jonah" href="/">Jonah Sussman</a>
+  <ul id="nav-menu">
+    <li><a class="nav-link" href="/">Home</a></li>
+    <li><a class="nav-link" href="/about">About</a></li>
+    <li><a class="nav-link" href="/projects">Projects</a></li>
+    <li><a class="nav-link" href="/assets/Jonah%20Sussman%20Resume%20ONLINE%202020-12-20.pdf">Resume</a></li>
+  </ul>
+</div>
 `;
-
-// let colors = [
-//   'hsl(171, 100%, 41%)',
-//   'hsl(217, 71%, 53%)',
-//   'hsl(204, 86%, 53%)',
-//   'hsl(141, 53%, 53%)',
-//   'hsl(48, 100%, 67%)',
-//   'hsl(348, 100%, 61%)'
-// ];
-
-// let arr = [];
-
-// while (arr.length < 3) {
-//   let r = 0;
-//   do {
-//     r = rb(0, colors.length - 1);
-//   } while (arr.includes(r));
-//   arr.push(r);
-// }
-
-// let css = /* css */`.crazy-jonah:hover { text-shadow: ${rb(-9, 9)}px ${rb(-9, 9)}px ${colors[arr[0]]}, ${rb(-9, 9)}px ${rb(-9, 9)}px ${colors[arr[1]]}, ${rb(-9, 9)}px ${rb(-9, 9)}px ${colors[arr[2]]};}`;
 
 class JNav extends HTMLElement {
   constructor() {
     super();
-    this.className = "mainnav responsive";
-    this.id = "mainnav";
+    this.className = "";
+    this.id = "navbar";
     this.innerHTML = html;
 
-    // let styleSheet = document.createElement("style");
-    // styleSheet.type = "text/css";
-    // styleSheet.innerText = css;
-    // document.head.appendChild(styleSheet);
-
-    document.getElementById("linavburger").addEventListener("click", navResponse, false);
+    document.getElementById("nav-toggle").addEventListener("click", navResponse, false);
   }
 }
 
